5db9dc19bb96aea190b74e023b959a1657a5b059
Bailey Bakerson <bailey.bakerson@gmail.com>
2023-11-15 19:04:42 -0500

    heart

--- heart.py
+++ heart.py
@@ -1,6 +1,17 @@
-print('\n'.join
-      ([''.join
-        ([('Engineer'[(x-y)%8]
-           if((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3<=0
-           else' ')for x in range(-30,30)])
-           for y in range(15,-15,-1)]))
+word = 'ThisIsWayTooLongToBeAWord'
+wordLen = len(word)
+
+width = 0.05 # 
+height = 0.1 # ~0.08-0.2
+
+areaX = int((1 - width) * 32)
+areaY = int((1 - height) * 17)
+print(areaX, areaY)
+
+print('\n'.join([
+  ''.join([
+    (word[(x-y)%wordLen] if((x*width)**2+(y*height)**2-1)**3-(x*width)**2*(y*height)**3<=0 else' ') 
+    for x in range(-areaX,areaX)
+  ]) 
+  for y in range(areaY,-areaY,-1)
+]))
